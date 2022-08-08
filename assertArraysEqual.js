2813b5fa92c52a05bffe6cf4f65e80b7d437430b
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-08-08 15:20:03 +0000

    Refactored middle and created a separate file middleTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,10 +9,10 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertArraysEqual;
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+/* assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false */
